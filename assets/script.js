3032c78d492f989850a97b37de1119655aefc278
Lammyy5 <reliford2010@gmail.com>
2023-09-13 10:11:04 -0700

    Added date and time

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,23 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+// Elements
+const currentDayEl = $("#currentDay");
+
 $(function () {
-    // TODO: Add a listener for click events on the save button. This code should
+  
+  // WHEN I open the planner
+  // THEN the current day is displayed at the top of the calendar
+  function dateTime() {
+    const currentDayEl = $("#currentDay");
+  var currentDay = dayjs().format("MMM DD, YYYY [at] hh:mm:ss a")
+    currentDayEl.text(currentDay);
+  }
+  setInterval(dateTime,1000);
+  });
+  
+
+  // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
     // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -20,5 +35,3 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
-  
